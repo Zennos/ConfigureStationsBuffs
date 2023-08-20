3a4dd991d081a766e170e3f0a3d736251bb50693
Zennos <edoan.rodrigues@gmail.com>
2023-08-20 20:27:56 -0300

    remove buff infinite duration

--- LongerStationsBuffs.cs
+++ LongerStationsBuffs.cs
@@ -1,20 +1,33 @@
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace LongerStationsBuffs
 {
 	public class LongerStationsBuffs : Mod
 	{
+
+		bool isInfiniteBuff = true;
+
 		public override void Load()
 		{
-			Terraria.On_Player.AddBuff += Player_AddBuff;
-		}
+            Terraria.On_Player.AddBuff += Player_AddBuff;
+        }
+
+        public override void Unload()
+        {
+			SetInfiniteTimeFromBuffs(false);
+        }
 
         private void Player_AddBuff(Terraria.On_Player.orig_AddBuff orig, Player self, int type, int timeToAdd, bool quiet, bool foodHack)
 		{
 			LongerStationsBuffsConfig modConfig = ModContent.GetInstance<LongerStationsBuffsConfig>();
-			if (modConfig.ModEnabled)
+
+            if (modConfig.ModEnabled)
 			{
+				if (this.isInfiniteBuff) SetInfiniteTimeFromBuffs(false);
+
 				bool changed = true;
 				if (modConfig.customBuffDuration.enabled)
 				{
@@ -22,7 +35,7 @@ namespace LongerStationsBuffs
 					{
 						case Terraria.ID.BuffID.AmmoBox:
 							timeToAdd = modConfig.customBuffDuration.AmmoBox;
-							break;
+                            break;
 						case Terraria.ID.BuffID.Bewitched:
 							timeToAdd = modConfig.customBuffDuration.BewitchingTable;
 							break;
@@ -39,7 +52,8 @@ namespace LongerStationsBuffs
 							changed = false;
 							break;
 					}
-				}
+
+                }
 				else
 				{
 					switch (type)
@@ -59,13 +73,30 @@ namespace LongerStationsBuffs
 				if (changed)
 				{
 					timeToAdd = timeToAdd * 60 * 60;
-				}
-			}
+                }
+			} else if (!this.isInfiniteBuff)
+            {
+                SetInfiniteTimeFromBuffs(true);
+            }
 
 			orig.Invoke(self, type, timeToAdd, quiet, foodHack);
 		}
 
-		public static bool IsPlayerLocalServerOwner(Player player)
+		public void SetInfiniteTimeFromBuffs(bool state)
+		{
+			var buffIds = new [] { BuffID.AmmoBox, BuffID.Bewitched, BuffID.Clairvoyance, BuffID.Sharpened, BuffID.Clairvoyance };
+
+			for (int i = 0; i < buffIds.Length; i++)
+			{
+				var buffId = buffIds[i];
+				BuffID.Sets.TimeLeftDoesNotDecrease[buffId] = state;
+				Main.buffNoTimeDisplay[buffId] = state;
+            }
+
+			this.isInfiniteBuff = state;
+		}
+
+        public static bool IsPlayerLocalServerOwner(Player player)
 		{
 			if (Main.netMode == Terraria.ID.NetmodeID.MultiplayerClient)
 			{
